8afeba8311d6f29ce54081fbdf2d9ce5c203343e
Florian Ruhnke <florian+git@ruhnke.cloud>
2020-02-18 15:05:01 +0100

    Update ident_me.pl

--- ident_me.pl
+++ ident_me.pl
@@ -24,31 +24,21 @@ if ($file eq "y") {
     chomp $outputfile;
     ($outputfile = "./ident.txt") if ($outputfile eq "");
     open my $OUT, '>', $outputfile or die "Can't open file: $!\nDo you have write access to the file?";
-    print $OUT "Your external IPv4 is $v4\n";
-    print $OUT "Your external IPv6 is $v6\n\n";
-    print $OUT "PTR-Record for $v4 is:\n",`nslookup $v4`,"\n";
-    print $OUT "PTR-Record for $v6 is:\n",`nslookup $v6`,"\n";
-    print $OUT "whois of $v4:\n",`whois $v4`,"\n";
-    print $OUT "whois of $v6:\n",`whois $v6`,"\n";
+    print $OUT "Your external IPv4 is $v4\n","Your external IPv6 is $v6\n\n","PTR-Record for $v4 is:\n",`nslookup $v4`,"\n","PTR-Record for $v6 is:\n",`nslookup $v6`,"\n","whois of $v4:\n",`whois $v4`,"\n","whois of $v6:\n",`whois $v6`,"\n";
     close $OUT;
     print "Done. See ",$outputfile," for the result.\n"
 } elsif ($file eq "n") {
-    print "Your external IPv4 is $v4\n";
-    print "Your external IPv6 is $v6\n\n";
-    print "Press 'q' to exit or any other key for PTR-Records\n";
+    print "Your external IPv4 is $v4\n","Your external IPv6 is $v6\n\n","Press 'q' to exit or any other key for PTR-Records\n";
     ReadMode('cbreak');
     $key = ReadKey(0);
     ReadMode('normal');
     (exit (0)) if ($key eq "q");
-    print "PTR-Record for $v4 is:\n",`nslookup $v4`,"\n";
-    print "PTR-Record for $v6 is:\n",`nslookup $v6`,"\n";
-    print "Press 'q' to exit or any other key for 'whois $v4'\n";
+    print "PTR-Record for $v4 is:\n",`nslookup $v4`,"\n","PTR-Record for $v6 is:\n",`nslookup $v6`,"\n","Press 'q' to exit or any other key for 'whois $v4'\n";
     ReadMode('cbreak');
     $key = ReadKey(0);
     ReadMode('normal');
     (exit (0)) if ($key eq "q");
-    print "whois of $v4:\n",`whois $v4`,"\n";
-    print "Press 'q' to exit or any other key for 'whois $v6'\n";
+    print "whois of $v4:\n",`whois $v4`,"\n","Press 'q' to exit or any other key for 'whois $v6'\n";
     ReadMode('cbreak');
     $key = ReadKey(0);
     ReadMode('normal');
